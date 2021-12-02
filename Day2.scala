ac92445c9a1ac78112dd019440685e304f66d859
gstravinskaite <g.stravinskaite@elsevier.com>
2021-12-02 17:21:41 +0100

    Day2 part 2

--- Day2.scala
+++ Day2.scala
@@ -21,7 +21,29 @@ object Day2 {
             e.split(" ")(1).toInt
       ).foldLeft(0)(_+_)
 
-    println(sum_depth * sum_forward)
+    println(s"The solution number one is: ${sum_depth * sum_forward}")
+
+// Part 2: the order matters
+    // forward - go ahead AND go deeper by ahead*aim
+    // up - reduce aim
+    // down - increase aim
+
+    // function to get depth
+
+    var depth = 0
+    var aim = 0
+
+    for (i <- lines) {
+
+      val sep_elem = i.split(" ")
+      sep_elem(0) match {
+        case "forward" => depth += sep_elem(1).toInt * aim
+        case "up" => aim -= sep_elem(1).toInt
+        case "down" => aim += sep_elem(1).toInt
+      }
+    }
+
+    println(s"The solution number two is: ${depth * sum_forward}")
 
   }
 
