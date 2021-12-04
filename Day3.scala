c3e97cfae62ddd23b4c028cbf66673f8e1866ee6
gstravinskaite <g.stravinskaite@elsevier.com>
2021-12-04 17:36:03 +0100

    shitty Day3 part 2 solution :(

--- Day3.scala
+++ Day3.scala
@@ -1,37 +1,100 @@
 import jdk.internal.util.xml.impl.Input
 
 object Day3 {
+
   def main(args: Array[String]): Unit = {
-    val input = loadData("input_big.txt")
-    val gamma = gamma_binnary(input)
-    val power_consumption = get_power_consumption(gamma)
-    println(power_consumption)
+    val power_consumption = get_power_consumption("input_big.txt")
+
+    val oxygen = get_oxygen_generator_rating("input_big.txt")
+    println(oxygen)
+
+    val co2 = get_co2_rating("input_big.txt")
+    println(co2)
+
+    println(oxygen*co2)
+
   }
 
-
-  def loadData(filePath: String): List[List[Char]] = {
+  def loadData(filePath: String): List[List[String]] = {
     val bufferedSource = io.Source.fromFile(filePath)
-    val lines = (for (line <- bufferedSource.getLines()) yield line.toList).toList
+    val lines = (for (line <- bufferedSource.getLines()) yield line.toList.map(e => e.toString)).toList
     bufferedSource.close
 
-    lines.transpose
+    lines
   }
 
-  def gamma_binnary(input: List[List[Char]]): List[String] = {
-    // identity == x => x -- i.e. group by every character
-    // maxby return only a tuple with the max value and its instance
-    val gamma = input.map{
+  def get_power_consumption(filePath: String): Int = {
+
+    val input = loadData(filePath).transpose
+
+    val gamma_binary = input.map{
       x => x.groupBy(identity).maxBy(_._2.size)._1.toString
     }
-    gamma
-  }
-
-  def get_power_consumption(binary_num: List[String]): Int = {
-    val epsilon_binary = binary_num.map{
+    val epsilon_binary = gamma_binary.map{
         case "1" => "0"
         case "0" => "1"
     }
-    Integer.parseInt(binary_num.mkString(""), 2) * Integer.parseInt(epsilon_binary.mkString(""), 2)
+    Integer.parseInt(gamma_binary.mkString(""), 2) * Integer.parseInt(epsilon_binary.mkString(""), 2)
+  }
+
+/// other metrics
+  // need to get the first numbers of each list, got the most common, discard those with no most common number
+
+  def get_oxygen_generator_rating(filePath: String): Int = {
+    var input = loadData(filePath)
+    var input_trans = input.transpose
+
+    println(input.filter(x => x(i) == "1"))
+    // try to get an implicit iterator with the collect stuff
+    for (i <- 0 to (input(0).length-1)) {
+      val frequence_map = input_trans(i).groupBy(identity)
+
+
+      var most_common_number = "1"
+      if (frequence_map.contains("0") & frequence_map.contains("1")) {
+        val zeros = frequence_map("0").size
+        val ones = frequence_map("1").size
+
+        if (zeros > ones) {
+          most_common_number = "0"
+        }
+      } else {
+        most_common_number = frequence_map.maxBy(_._2.size)._1.toString()
+      }
+
+      input = input.filter(x => x(i) == most_common_number)
+      input_trans = input.transpose
+    }
+    Integer.parseInt(input(0).mkString(""), 2)
+  }
+
+  def get_co2_rating(filePath: String): Int = {
+    var input = loadData(filePath)
+    var input_trans = input.transpose
+
+
+    // try to get an implicit iterator with the collect stuff
+    for (i <- 0 to (input(0).length-1)) {
+      val frequence_map = input_trans(i).groupBy(identity)
+
+      var least_common_number = "0"
+      if (frequence_map.contains("0") & frequence_map.contains("1")) {
+        val zeros = frequence_map("0").size
+        val ones = frequence_map("1").size
+
+        if (zeros > ones) {
+          least_common_number = "1"
+        }
+      } else {
+        least_common_number = frequence_map.minBy(_._2.size)._1.toString()
+      }
+
+
+      input = input.filter(x => x(i) == least_common_number)
+      input_trans = input.transpose
+    }
+
+    Integer.parseInt(input(0).mkString(""), 2)
   }
 
 
