ac4b180e0378f68520fab0cf9f04558f96a7bd4a
gstravinskaite <g.stravinskaite@elsevier.com>
2022-03-14 13:12:43 +0100

    Dawkins Weasel

--- src/main/shekspearmonkeys/Monkey.scala
+++ src/main/shekspearmonkeys/Monkey.scala
@@ -1,27 +1,90 @@
 package shekspearmonkeys
+import scala.annotation.tailrec
 import scala.util.Random
 
-case class Computer(guess:String){
-  val TARGET = "METHINKS IT IS LIKE A WEASEL"
-  def isCorrect = ???
-  def score = ???
-  def pickBestGuess = ???
+trait SimulationStatus
+case object Correct extends SimulationStatus
+case object InProgress extends SimulationStatus
+
+case class ShakespeareSimulation(status: SimulationStatus, monkey: Monkey, computer: Computer = Computer()) {
+
+  def nextState(): ShakespeareSimulation = {
+    status match {
+      case Correct => null
+      case InProgress =>
+        val monkeyGuess = monkey.copyAndMutate()
+        val (bestString, score)  = computer.pickBestGuess(monkeyGuess)
+        println(f"Best GUESS: $bestString")
+
+        val newStatus = score match {
+          case 28 => Correct
+          case _ => InProgress
+        }
+        copy(status = newStatus, monkey = Monkey(bestString))
+    }
+  }
 }
 
-case class Monkey(initialGuess: String){
+case class Computer() {
+  val TARGET = "METHINKS IT IS LIKE A WEASEL"
+
+  private def score(givenGuess: String): Int = {
+    val isCharCorrect =  (0 to givenGuess.length-1).map(
+      e => if (givenGuess(e)== TARGET(e)) 1 else 0
+    )
+    isCharCorrect.foldLeft(0)(_+_)
+  }
+
+  def pickBestGuess(givenGuesses: Seq[String]): (String, Int) = {
+    val scores = givenGuesses.map(e => score(e))
+    val bestScoreIndex = scores.indexOf(scores.max)
+    (givenGuesses(bestScoreIndex), scores.max)
+  }
+}
+
+case class Monkey(guess: String){
+
   val KEYBOARD = Seq("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
     "T", "U", "V", "W", "X", "Y", "Z", " ")
   val numberOfGuesses = 100
-  val probabilities = (1 to numberOfGuesses).map(_ => Random.nextInt(initialGuess.length))
+  // this needs to be done as a def so it would be dynamic
+  private def generateProbabilities =
+    (1 to numberOfGuesses).map(_ => Random.nextInt(numberOfGuesses))
+
   // Can we define that the length is 100?
-  def copyAndMutate(guess: String): Seq[String] = {
-    val copiedGuess = (1 to numberOfGuesses).map(_ => guess)
+  def copyAndMutate(givenGuess: String = guess): Seq[String] = {
+    val copiedGuess = (1 to numberOfGuesses).map(_ => givenGuess)
     copiedGuess.map(e => mutateGuess(e))
   }
-  def mutateGuess(guess: String, probs: Seq[Int] = probabilities): String = {
+
+  private [shekspearmonkeys] def mutateGuess(givenGuess: String = guess, probs: Seq[Int] = generateProbabilities): String = {
     // nextInt is end exclusive
-    val zipped = guess zip probs
+    val zipped = givenGuess zip probs
     val mutated = zipped.map(e => if (e._2<5) KEYBOARD(Random.nextInt(KEYBOARD.length)) else e._1)
     mutated.mkString("")
   }
+}
+
+object ShakespeareSimulation{
+
+  @tailrec
+  private def nextValidGuess(): String = {
+    println("Enter 28 symbol input")
+    val initialGuess = scala.io.StdIn.readLine().toUpperCase
+    initialGuess.length match {
+      case 28 => initialGuess
+      case _ =>
+        println("The input has to be 28 symbols long")
+        nextValidGuess()
+    }
+  }
+  def main(args: Array[String]): Unit = {
+   // "SKTOFPRHTISHROPWRNT O RUFNT "
+    val initialGuess = nextValidGuess()
+
+    val simulation = ShakespeareSimulation(InProgress, Monkey(initialGuess))
+    val states = Iterator.iterate(simulation)(_.nextState()).takeWhile(_ != null)
+    println(f"The correct state reached in ${states.length} steps")
+    }
+
 }
--- src/test/shekspearmonkeys/Monkey.scala
+++ src/test/shekspearmonkeys/Monkey.scala
@@ -4,8 +4,12 @@ import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
 
 class MonkeyTest extends AnyFlatSpecLike  with Matchers {
-  val GUESS = "ABCDEFGHIJKLMNOPPPBBBBBBBBBB"
+
+  val GUESS = "METHINKS HE IS LIKE A WEASEL"
+  val GUESS_TWO = "METHINKS HEBIS LIKE A WEASEL"
   val testMonkey = Monkey(GUESS)
+  val testComputer = Computer()
+
   it should "multiply the guesses " in {
     val guesses = testMonkey.copyAndMutate(GUESS)
     guesses.length shouldBe 100
@@ -14,8 +18,17 @@ class MonkeyTest extends AnyFlatSpecLike  with Matchers {
   it should "mutate the guess" in {
     val testProbs = Seq(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27)
     val mutated = testMonkey.mutateGuess(GUESS, testProbs)
+
     mutated.slice(5, GUESS.length) shouldBe GUESS.slice(5, GUESS.length)
     mutated.slice(0,4) should not be GUESS.slice(0, 4)
   }
+
+
+  it should "pick the best string" in {
+    val (bestGess, score) = testComputer.pickBestGuess(Seq(GUESS, GUESS_TWO))
+    bestGess shouldBe GUESS
+    score shouldBe 26
+  }
 }
+
 
